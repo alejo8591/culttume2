5d73ce7e24cfb5ed6fc8a5848d1585d93d20ced3
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-04 18:36:46 -0500

    first test with facebook login with django-social-auth

--- culttume/urls.py
+++ culttume/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView, RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'culttume.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('social_auth.urls')),
+    url(r'^login/$', RedirectView, {'url': '/login/facebook'}),
+    #url(r'^private/$', 'home.views.private'),
 )
