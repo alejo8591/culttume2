30f7bada5396813e876712a1b2ae92fe28862d14
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-04 19:26:42 -0500

    update comments in urls.py

--- culttume/urls.py
+++ culttume/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'culttume.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # Migrating function-based generic views - https://docs.djangoproject.com/en/1.4/topics/generic-views-migration/
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
