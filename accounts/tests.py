465db23f672240364248a007541587c81209b9d6
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-25 16:56:07 -0500

    Update with "HomeTest" test case with selenium

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+from selenium.webdriver.common.keys import Keys
+
+from django.test import LiveServerTestCase
+
+# Test Home Page
+class HomeTest(LiveServerTestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_home_site(self):
+		# user open web browser, navigation to home page
+		self.browser.get(self.live_server_url + '/')
+
+		body = self.browser.find_element_by_tag_name('body')
+		self.assertIn('Login with Facebook', body.text)
